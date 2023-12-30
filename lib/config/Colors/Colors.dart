c5178e95e5aea8e374765bb2ffb2d47f17132acc
SubhagyaBorus <135357408+SubhagyaBorus@users.noreply.github.com>
2023-12-30 17:19:27 +0530

    update

--- lib/config/Colors/Colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static Color LightPink = Color.fromRGBO(220, 220, 220, 60);
-  static Color seashell = Color.fromRGBO(255, 245, 238, 80);
-  static Color blanchedalmond = Color.fromRGBO(255, 235, 205, 160);
-  static Color lightcyan = Color.fromRGBO(224, 255, 255, 150);
-  static Color thistle = Color.fromRGBO(220, 220, 220, 200);
-}
